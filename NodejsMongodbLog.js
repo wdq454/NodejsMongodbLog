8309ec0aed9bb253fbb83dae808841468aa25a9b
wdq454 <2502286740@qq.com>
2021-12-08 17:36:47 -0800

    commit1

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,18 @@
 const express =require("express")
 const app=express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test1');
+
+const schema={
+    name:String,
+    age:Number,
+    health:String,
+    hooby:String
+}
+const mydata = mongoose.model('cat1', schema);
+const kitty = new mydata({ name: 'testZan3' });
+kitty.save()
+
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
